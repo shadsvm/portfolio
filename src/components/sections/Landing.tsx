481addf2604360d294aa14c6175aba633ac856a2
Samuel <91400290+sam-kmn@users.noreply.github.com>
2023-06-30 19:02:12 +0200

    ✨ Social buttons on landing

--- src/components/sections/Landing.tsx
+++ src/components/sections/Landing.tsx
@@ -1,7 +1,8 @@
+import { BsGithub, BsLinkedin } from "react-icons/bs"
 import SectionWrapper from "../SectionWrapper"
+import Image from "next/image"
 import { motion } from "framer-motion"
 import { fadeIn, textVariant } from "@/utils/motion"
-import Image from "next/image"
 
 const Landing = () => {
   return (
@@ -22,6 +23,27 @@ const Landing = () => {
             <br /> with well-versed knowledge of Vue, React,
             <br /> TypeScript, and many other technologies.
           </motion.p>
+          <div className="mt-5 flex gap-10 text-xl sm:text-2xl">
+            <motion.a
+              variants={fadeIn({ delay: 0.6, duration: 1 })}
+              href="https://www.github.com/sam-kmn"
+              target="_blank"
+              className="flex items-center gap-3 transition hover:scale-105 hover:text-primary-500"
+            >
+              <BsGithub className="text-3xl sm:text-4xl" />
+              GitHub
+            </motion.a>
+
+            <motion.a
+              variants={fadeIn({ delay: 0.7, duration: 1 })}
+              href="https://www.Linkedin.com/in/sam-kmn"
+              target="_blank"
+              className="flex items-center gap-3 transition hover:scale-105 hover:text-primary-500"
+            >
+              <BsLinkedin className="text-3xl sm:text-4xl" />
+              Linkedin
+            </motion.a>
+          </div>
         </div>
         <motion.div
           className="relative hidden h-72 w-1/4 rounded-2xl md:flex lg:h-96 lg:w-1/3 "
